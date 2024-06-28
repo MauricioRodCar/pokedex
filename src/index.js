59e3d8396e73b4dbf4cd033c144495bbaf422886
MauricioRodCar <mauriciorodricar@hotmail.com>
2024-06-28 17:54:56 -0600

    Removed reference emails

--- src/index.js
+++ src/index.js
@@ -26,6 +26,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-//merilyn@densitylabs.io
-//ivan.fuentes@densitylabs.io
